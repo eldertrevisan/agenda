ef893fcc66cf68774201cd49135e150d11d73a48
elder trevisan <trevisan.elder@gmail.com>
2016-03-30 13:55:08 -0300

    Várias correções e melhorias no código

--- app.py
+++ app.py
@@ -1,101 +1,87 @@
 ﻿# -*- coding: utf-8 -*-
-from bottle import redirect, route, static_file, error, run
-from bottle import run, request, jinja2_template as template
+#------------------------------------------------------------------------------
+# Author:      Elder Sanitá Trevisan
+#
+# Copyright(©) 2016 Elder Sanitá Trevisan
+# Licence:     GPL
+#------------------------------------------------------------------------------
+from bottle import redirect, route, static_file, error, request, run,\
+	jinja2_template as template
 import os
-import sqlite3
+import html
+import model
+import re
 
-dir = os.path.dirname(__file__)
-banco = filename = os.path.join(dir, 'agenda.db')
+BASEDIR = os.path.dirname(__file__)
+BANCO = os.path.join(BASEDIR, 'agenda.db')
+
 
 @route('/')
 @route('/index')
 def index():
-	conn = sqlite3.connect(banco)
-	cursor = conn.cursor()
-	cursor.execute("SELECT * FROM agenda")
-	result = cursor.fetchall()
-	conn.close()
-	return template("index.html", data=sorted(result, key=lambda tup: tup[1]))
+	contatos = model.ler_todos_contatos()
+	return template("index.html", data=contatos)
 
 @route('/criar_contato', method=['GET', 'POST'])
 def criar_contato():
-	if request.forms.get('save'):
-		nome = request.forms.get('nome')
-		telefone = request.forms.get('telefone')
-		logradouro = request.forms.get('logradouro')
-		numero = request.forms.get('numero')
-		complemento = request.forms.get('complemento')
-		bairro = request.forms.get('bairro')
-		cidade = request.forms.get('cidade')
-		uf = request.forms.get('uf')
+	if request.method == 'POST':
+		nome = html.escape(request.forms.get('nome'))
+		telefone = html.escape(request.forms.get('telefone'))
+		telefone = re.sub(r'\D', "", telefone)
+		logradouro = html.escape(request.forms.get('logradouro'))
+		numero = html.escape(request.forms.get('numero'))
+		numero = re.sub(r'\D', "", numero)
+		complemento = html.escape(request.forms.get('complemento'))
+		bairro = html.escape(request.forms.get('bairro'))
+		cidade = html.escape(request.forms.get('cidade'))
+		uf = html.escape(request.forms.get('uf'))
 		
-		try:
-			conn = sqlite3.connect(banco)
-			cursor = conn.cursor()
-			cursor.execute('''INSERT INTO agenda
-							VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
-							''', (None, nome, telefone, logradouro, numero,\
-							complemento, bairro, cidade, uf))
-			conn.commit()
-			conn.close()
-			redirect('/index')
-		except sqlite3.Error as erro:
-			if erro.args[0] == "UNIQUE constraint failed: agenda.nome":
-				return "<script>alert('Este contato já existe na agenda!')"\
-				"</script>", "<script>window.history.back()</script>"
-			else:
-				return "<p>Ocorreu o seguinte erro: ",erro.args[0]," </p>"
+		d = model.inserir_contato(nome, telefone, logradouro, numero,\
+		complemento, bairro, cidade, uf)
+		
+		if d != None:
+			return "<script>alert('",d,"'); window.history.back();</script>"
+		else:
+			redirect('/')
 	else:
 		return template('criar_contato.html')
 
 @route('/contato/<pk:int>', method='GET')
 def contato(pk):
-	conn = sqlite3.connect(banco)
-	cursor = conn.cursor()
-	cursor.execute('''SELECT * FROM agenda WHERE id=?''',(pk,))
-	data = cursor.fetchall()
-	conn.close()
-	return template('contato.html', data=data)
+	pk = int(pk)
+	contato = model.ler_contato(pk)
+	return template('contato.html', data=contato)
 	
 @route('/contato/edita_contato/<pk:int>', method=['GET','POST'])
 def edita_contato(pk):
-	if request.forms.get('save'):
-		nome = request.forms.get('nome')
-		telefone = request.forms.get('telefone')
-		logradouro = request.forms.get('logradouro')
-		numero = request.forms.get('numero')
-		complemento = request.forms.get('complemento')
-		bairro = request.forms.get('bairro')
-		cidade = request.forms.get('cidade')
-		uf = request.forms.get('uf')
-		try:
-			conn = sqlite3.connect(banco)
-			cursor = conn.cursor()
-			cursor.execute('''UPDATE agenda SET nome=?, telefone=?,
-			logradouro=?, numero=?, complemento=?, bairro=?, cidade=?, uf=?
-			WHERE id=?''',(nome, telefone, logradouro, numero, complemento,\
-			bairro, cidade, uf, pk,))
-			conn.commit()
-			conn.close()
-			redirect('/index')
-		except sqlite3.Error as erro:
-			return "<script>alert('Não foi possível alterar o contato, ocorreu"\
-			"o seguinte erro: ",erro.args[0],"')</script>",\
-			"<script>window.history.back()</script>"
+	pk = int(pk)
+	if request.method == 'POST':
+		nome = html.escape(request.forms.get('nome'))
+		telefone = html.escape(request.forms.get('telefone'))
+		telefone = re.sub(r'\D', "", telefone)
+		logradouro = html.escape(request.forms.get('logradouro'))
+		numero = html.escape(request.forms.get('numero'))
+		numero = re.sub(r'\D', "", numero)
+		complemento = html.escape(request.forms.get('complemento'))
+		bairro = html.escape(request.forms.get('bairro'))
+		cidade = html.escape(request.forms.get('cidade'))
+		uf = html.escape(request.forms.get('uf'))
+		
+		d = model.editar_contato(nome, telefone, logradouro, numero,\
+		complemento, bairro, cidade, uf, pk)
+		
+		if d != None:
+			return "<script>alert('",d,"'); window.history.back();</script>"
+		else:
+			redirect('/')
+	else:
+		return template('criar_contato.html')
 	
 @route('/contato/exclui_contato/<pk:int>')
 def exclui_contato(pk):
-	try:
-		conn = sqlite3.connect(banco)
-		cursor = conn.cursor()
-		cursor.execute('''DELETE FROM agenda WHERE id=?''',(pk,))
-		conn.commit()
-		conn.close()
-		redirect('/index')
-	except sqlite3.Error as erro:
-		return "<script>alert('Não foi possível excluir, ocorreu o seguinte "\
-		"erro: ",erro.args[0],"')</script>",\
-		"<script>window.history.back()</script>"
+	pk = int(pk)
+	model.excluir_contato(pk)
+	redirect('/')
 
 @route('/static/<filename:path>')
 def server_static(filename):
@@ -106,4 +92,5 @@ def error404(error):
 	return "Ops, página não encontrada!"
 
 
-run(reloader=False, debug=False)
+if __name__ == "__main__":
+	run(reloader=True, debug=True)
